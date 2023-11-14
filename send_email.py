4f36f48fac10dc878fb1272b6250086398c67894
Joseph Ofei <josephofei24@gmail.com>
2023-11-14 14:19:43 -0500

    project is done

--- send_email.py
+++ send_email.py
@@ -1,4 +1,5 @@
 import smtplib, ssl
+import os
 
 
 def send_email(message):
@@ -6,7 +7,7 @@ def send_email(message):
     port = 465
 
     username = 'josephofei22@gmail.com'
-    password = 'hqhjkgkccjqquenh'
+    password = os.getenv("PASSWORD")
 
     receiver = "josephofei22@gmail.com"
     context = ssl.create_default_context()
